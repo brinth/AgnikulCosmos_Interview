7926c976523dbff37d7058594122826ad4c099e4
Brinth Khanna <brinthkhanna@gmail.com>
2021-01-27 21:32:09 +0530

    Add files via upload

--- SX1272/SX1272.c
+++ SX1272/SX1272.c
@@ -9,7 +9,46 @@
 
 */
 
-bool LoRa_Init(){
+#include <SX1272.h>
+
+//Assuming the Freq of oscillator Fosc = 8 MHz
+#define __OSC_FREQ 8000000
+
+bool LoRa_Init(unsigned char _mode, unsigned int _freq,
+				RegOpMode_Typedef RegOpMode_Val,
+				RegModemConfig1_Typedef RegModemConfig1_Val,
+				RegModemConfig2_Typedef RegModemConfig2_Val
+				)
+{
+	/*
+	MODE Settings
+	*/
 	
-	
+	//Put Module in Sleep Mode to set Module in LoRa Mode
+	unsigned char mode = RegOpMode_Val.Mode
+	RegOpMode_Val.Mode = SLEEP_MODE;
+	SPI_ReadAndWriteByte(RegOpMode_Val,0);
+	RegOpMode_Val.Mode = mode;
+	SPI_ReadAndWriteByte(RegOpMode_Val,0);
+	SPI_ReadAndWriteByte(RegModemConfig1_Val,0);
+	SPI_ReadAndWriteByte(RegModemConfig2_Val,0);
 }
+
+/*
+	Sample Implementation:
+	
+	#include<SX1272.h>
+	RegOpMode_Typedef RegOpMode_Val,
+	RegModemConfig1_Typedef RegModemConfig1_Val,
+	RegModemConfig2_Typedef RegModemConfig2_Val
+	RegOpMode_Val.LongRangeMode = LORA_MODE;
+	RegOpMode_Val.AccessSharedReg = ACCESS_LORA_REG;
+	RegModemConfig1_Val.Bw ...
+	RegModemConfig1_Val.CodingRate ...
+	RegModemConfig1_Val.ImplicitHeaderModeOn ...
+	RegModemConfig1_Val.RxPayloadCrcOn ...
+	RegModemConfig1_Val.LowDataRateOptimize ...
+	void main(){
+		LoRa_Init( ...
+	}
+*/
--- SX1272/SX1272.h
+++ SX1272/SX1272.h
@@ -15,10 +15,101 @@
 #include <SPI.h>
 #include <stdbool.h>
 
+/*
+	MACROS
+*/
+//LongRangeMode 
+#define LORA_MODE 1
+#define FSK_OOK_MODE 0
+//Access Shared RegOpMode
+#define ACCESS_LORA_REG 0
+#define ACCESS_FSK_REG 1
+//LoRa Modes
+#define SLEEP_MODE		0
+#define STDBY_MODE		1
+#define FSTX_MODE		2
+#define TX_MODE			3
+#define FSRX_MODE		4
+#define RX_CONT_MODE	5
+#define RX_SING_MODE    6
+#define CAD_MODE		7
+
+enum EN_REGISTERS_BASE_ADDRESS{
+	RegOpMode      		= 0x01;
+	RegFrMsb       		= 0x06;
+	RegFrMib       		= 0x07;
+	RegFrLsb       		= 0x08;
+	RegPaConfig    		= 0x09;
+	RegPaRamp	   		= 0x0A;
+	RegOcp		      	= 0x0B;
+	RegLna            	= 0x0C;
+	RegFifoAddrPtr    	= 0x0D;
+	RegFifoTxBaseAddr 	= 0x0E;
+	RegFifoRxBaseAddr	= 0x0F;
+	RegFifoRxCurrent	= 0x10;
+	RegIrqFlagMask		= 0x11;
+	RegIrqFlag			= 0x12;
+	RegRxNbBytes		= 0x13;
+	RegRxHeaderCntVMsb  = 0x14;
+	RegRxHeaderCntVLsb  = 0x15;
+	RegRxPacketCntVMsb  = 0x16;
+	RegRxPacketCntVLsb  = 0x17;
+	RegModemStat		= 0x18;
+	RegPktSnrValue 		= 0x19;
+	RegPktRssiValue		= 0x1A;
+	RegRssiValue		= 0x1B;
+	RegHopChannel		= 0x1C;
+	RegModemConfig1		= 0x1D;
+	RegModemConfig2		= 0x1E;
+	RegSymbTimeOutLsb	= 0x1F;
+	RegPreambleMsb		= 0x20;
+	RegPreambleLsb		= 0x21;
+	RegPayloadLength	= 0x22;
+	RegMaxPayloadLength = 0x23;
+	RegHopPeriod		= 0x24;
+	RegFifoRxByteAdd	= 0x25;
+	RegFeiMsb			= 0x28;
+	RegFeiMid			= 0x29;
+	RegFeiLsb			= 0x2A;
+	RegRssiWideband		= 0x2C;
+	RegDetectOptimize	= 0x31;
+	RegInvertIQ			= 0x33;
+	RegDetectionThresld = 0x37;
+	RegSyncWord			= 0x39;
+}Reg_Base_Addr;
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-	bool LoRa_Init(void); 
+	/*
+		STRUCTURES
+	*/
+	typedef struct St_RegOpMode{
+		unsigned char LongRangeMode : 1;
+		unsigned char AccessSharedReg : 1;
+		unsigned char unused : 3;
+		unsigned char Mode : 3;
+	} RegOpMode_Typedef;
+	typedef struct St_RegModemConfig1{
+		unsigned char Bw : 2;
+		unsigned char CodingRate : 3;
+		unsigned char ImplicitHeaderModeOn : 1;
+		unsigned char RxPayloadCrcOn : 1;
+		unsigned char LowDataRateOptimize : 1;
+	} RegModemConfig1_Typedef;
+	typedef struct St_RegModemConfig2{
+		unsigned char SpreadingFactor : 4;
+		unsigned char TxContinuousMode : 1;
+		unsigned char AgcAutoOn : 1;
+		unsigned char SymbTimeout : 2;
+	} RegModemConfig2_Typedef;
+	
+	
+	/*
+	FUNCTION DECLARATION
+	*/
+	bool LoRa_Init(unsigned int freq, unsigned char mode); 
 	unsigned char LoRa_Send(unsigned char* data, unsigned char length);
 	unsigned char LoRa_Receive(unsigned char* pData, unsigned char* pLength);
 	
